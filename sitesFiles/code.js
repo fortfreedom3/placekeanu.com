861898abfd267e63e278a61ba06a850979d09aa5
Bennett <bjupfield@gmail.com>
2021-11-17 18:53:16 -0600

    copied from local master

--- sitesFiles/code.js
+++ sitesFiles/code.js
@@ -10,17 +10,52 @@ function formsubmit(e){
     const adjective = form.querySelector("#adjective").querySelector("input");
     const superlative = form.querySelector("#superlative").querySelector("input");
     const adverb = form.querySelector("#adverb").querySelector("input");
-    console.log(noun.value)
-    createMadLibs(noun, adjective, superlative, adverb);
-    summonKeanu(noun, adjective, superlative, adverb);
+    createMadLibs(noun.value, adjective, superlative, adverb);
+    summonKeanu(noun.value, adjective.value, superlative.value, adverb.value);
     // add save to db.json somewhere and possibly a delete button
-    noun.value = ""
+    noun.value = "";
+    adjective.value = "";
+    superlative.value = "";
+    adverb.value = "";
 }
 function summonKeanu(noun = 5, adjective = 8, superlative = "y", adverb = "n"){
     if(noun === 5 && adjective === 8 & superlative === "y" && adverb === "n"){
         //this is where the default fetch request for the initial picture will go, we change the default values to be whatever we want the picture to be
     }
     else{
+        fetch("http://localhost:3000/random-alphabet")
+        .then(r=>r.json())
+        .then(r=>{
+            const summonArr = [];
+            const arg = [...arguments];
+            arg.forEach(e=>{
+                summonArr.push(e.slice(-2))
+            })
+            console.log(summonArr)
+            for(let i = 0; i < 2; i++){
+                const a = summonArr[i].slice(-1).toLowerCase().charCodeAt(0) - 97;
+                const b = summonArr[i].slice(-2, 1).toLowerCase().charCodeAt(0) - 97;
+                console.log(a)
+                console.log(b)
+                summonArr[i] = r.alphabet[a] * r.alphabet[b];
+            }
+            if(r.alphabet2.includes(summonArr[2].slice(-1))){
+                summonArr[2] = "y"
+            }
+            else{
+                summonArr[2] = "";
+            }
+            if(r.alphabet2.includes(summonArr[3].slice(-1))){
+                summonArr[3] = "g"
+            }
+            else{
+                summonArr[3] = "";
+            }
+            return summonArr
+        })
+        .then(r=>{
+            console.log(r)
+        })
         //this is where the equations and then the fetch request would be
     }
 }
